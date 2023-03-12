eaff8a80b9013a9145d86b3201445015fa10e658
Sujeeth <sujeethkrr@gmail.com>
2023-03-12 23:16:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/api")
+      .then(response => response.json())
+      .then(data => {
+        setBackendData(data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {typeof backendData.student === "undefined" ? (
+        <p>Loading ... </p>
+      ) : (
+        backendData.student.map((name, i) => <p key={i}>{name}</p>)
+      )}
     </div>
   );
 }
